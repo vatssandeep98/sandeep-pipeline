27c9c61bdf3af12e5552e04179e96e1d6f9e560d
Sandeep Sharma <vats.sandeep98@gmail.com>
2026-06-15 07:04:55 +0530

    dd

--- rg.tf
+++ rg.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg1" {
   name = "aarav-rg"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "rg1" {
+  name = "tinku-rg"
+  location = "central india"
+}
