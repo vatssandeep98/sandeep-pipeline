4367b92c2fcd7b5e785d9fae001213c112957d4d
Sandeep Sharma <vats.sandeep98@gmail.com>
2026-06-17 08:43:37 +0530

    ssssa

--- rg.tf
+++ rg.tf
@@ -3,6 +3,12 @@ resource "azurerm_resource_group" "rg" {
   location = "east us"
 }
 
+resource "azurerm_resource_group" "rg" {
+  name = "dadri-mzn"
+  location = "east us"
+}
+
+
 # resource "azurerm_resource_group" "rg1" {
 #   name = "aarav-rg"
 #   location = "central india"
